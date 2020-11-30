26e5718e6a47e3f76a071281752633fe8ca42b3e
Timothy Coleman <timhc22@gmail.com>
2020-11-30 21:44:35 +0000

    Add verifier contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,12 @@
 const ConvertLib = artifacts.require("ConvertLib");
 const MetaCoin = artifacts.require("MetaCoin");
 const SimpleStorage = artifacts.require("./SimpleStorage.sol");
+const Verifier = artifacts.require("./Verifier.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, MetaCoin);
   deployer.deploy(MetaCoin);
   deployer.deploy(SimpleStorage);
+  deployer.deploy(Verifier);
 };
