390e0b6a06f404632f0e496a3a1f314157a62caf
Timothy Coleman <timhc22@gmail.com>
2020-12-14 19:09:53 +0000

    Enable passing in of host and token when getting web3

--- client/src/getWeb3.js
+++ client/src/getWeb3.js
@@ -1,16 +1,34 @@
 import Web3 from "web3";
 
-const getWeb3 = () =>
+/**
+ * getWeb3
+ *
+ * @param host
+ * @param token
+ * @returns {Promise<unknown>}
+ */
+const getWeb3 = (host, token) =>
   new Promise((resolve, reject) => {
     // Wait for loading completion to avoid race conditions with web3 injection timing.
     window.addEventListener("load", async () => {
+      if (host) {
+        try {
+          // e.g. "https://rinkeby.infura.io/YOUR_TOKEN_HERE"
+          // add code to add / if not there: todo https://stackoverflow.com/questions/41172772/how-to-add-jpg-to-end-of-string-if-it-doesnt-exist
+          const web3 = new Web3(new Web3.providers.HttpProvider(`${host}${token}`));
+          // await window.ethereum.enable(); // todo needed?
+          resolve(web3);
+        } catch (error) {
+          reject(error);
+        }
+      }
       // Modern dapp browsers...
-      if (window.ethereum) {
+      else if (window.ethereum) {
         const web3 = new Web3(window.ethereum);
         try {
           // Request account access if needed
           await window.ethereum.enable();
-          // Acccounts now exposed
+          // Accounts now exposed
           resolve(web3);
         } catch (error) {
           reject(error);
